c35a92e7d12fa556ffb7c9fd62e9e76642ea712a
willohrobbins <willoh_robbins@outlook.com>
2024-02-11 14:19:13 -0600

    name update clear confusion

--- grayscale_mp3.py
+++ grayscale_mp3.py
@@ -59,5 +59,5 @@ def process_folder(input_folder, output_folder):
 
 # Example usage
 input_folder = "grayscale"
-output_folder = "grayscale mp3"
+output_folder = "gray mp3"
 process_folder(input_folder, output_folder)
