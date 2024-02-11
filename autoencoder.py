0a522e01068341ce1d899bccdc86b5e7f51973e4
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-10 22:13:01 -0600

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -109,7 +109,7 @@ actual_input_shape = 128 * 427  # Adjusted based on Mel spectrogram size
 model = AudioAutoencoder(input_shape=actual_input_shape)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Model training on: " + str(device))
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
 criterion = nn.MSELoss()
 
 num_epochs = 1000
